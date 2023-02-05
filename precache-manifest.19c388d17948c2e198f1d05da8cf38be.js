2318930c81346996326f935544bd594da1c61971
FadeHack <shivammaheshwari658@gmail.com>
2023-02-05 14:31:57 +0530

    Updates

---
+++ precache-manifest.19c388d17948c2e198f1d05da8cf38be.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "eed17619caff5cf1bbfa",
+    "url": "/SM-Portfolio/static/css/main.dc7bf1d1.chunk.css"
+  },
+  {
+    "revision": "eed17619caff5cf1bbfa",
+    "url": "/SM-Portfolio/static/js/main.eed17619.chunk.js"
+  },
+  {
+    "revision": "a89266deb73d87b01859",
+    "url": "/SM-Portfolio/static/js/1.a89266de.chunk.js"
+  },
+  {
+    "revision": "c0b1bc3197d5e812a270",
+    "url": "/SM-Portfolio/static/js/2.c0b1bc31.chunk.js"
+  },
+  {
+    "revision": "073a3a061d108157a108",
+    "url": "/SM-Portfolio/static/js/runtime~main.073a3a06.js"
+  },
+  {
+    "revision": "f0811eb05d3bbdda1c18488bdbb1075f",
+    "url": "/SM-Portfolio/static/media/sm_logo.f0811eb0.png"
+  },
+  {
+    "revision": "138649f394300956ebde8aec8a15862c",
+    "url": "/SM-Portfolio/static/media/shivam.138649f3.png"
+  },
+  {
+    "revision": "72c8aaef7d57751bff5e80f0fe213e71",
+    "url": "/SM-Portfolio/static/media/tnd.72c8aaef.jpg"
+  },
+  {
+    "revision": "7add57593a9054a3134a36e12cca93a1",
+    "url": "/SM-Portfolio/static/media/spotify.7add5759.jpg"
+  },
+  {
+    "revision": "b9f4f583a1fdc9a2e0cb6e31065db87e",
+    "url": "/SM-Portfolio/static/media/textUt.b9f4f583.jpg"
+  },
+  {
+    "revision": "b82b1181c6cd5990c5486e2b5efb0a3b",
+    "url": "/SM-Portfolio/static/media/bank.b82b1181.jpg"
+  },
+  {
+    "revision": "10802cb3f7a82198e46b0090d9bc3204",
+    "url": "/SM-Portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.34ee59ba311aea8be5cbf6437dfcd25d.js"
+  "/SM-Portfolio/precache-manifest.19c388d17948c2e198f1d05da8cf38be.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/SM-Portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
