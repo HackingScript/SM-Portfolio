0c50696b07ac337d6060ff2380d79a12c85ff96c
FadeHack <shivammaheshwari658@gmail.com>
2023-02-05 15:05:25 +0530

    Configure React app for deployment to GitHub Pages

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -14,7 +14,7 @@ export default function Landing(props){
   
   const handleDownloadCVClick = () => {
     const link = document.createElement("a");
-    link.href = "documents/shivam_cv.pdf";
+    link.href = "/shivam_cv.pdf";
     link.download = "shivam_cv.pdf";
     link.click();
     props.showAlert("CV Downloaded", "success");
